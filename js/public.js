82a0045cc3f854d8a3e541e9dcaf244f2c1be48e
SaSaChu <teresa7397@gmail.com>
2016-11-23 18:09:10 +0800

    抓回 js.

--- js/public.js
+++ js/public.js
@@ -1,1 +1,21 @@
-$(function(){$(".Iaboutleft, .imgbox, .aboutbanner").imgLiquid({verticalAlign:"center"}),$(".tagbox.tag01, .tagbox.tag02").click(function(){$(".tagbox.tag01, .tagbox.tag02").removeClass("Caboutac"),$(this).addClass("Caboutac"),$(".Cabouttypebox").removeClass("Cabouttypeboxshow"),$(".Cabouttypebox").eq($(this).index()).addClass("Cabouttypeboxshow")}),$(".tagbox.tag01, .tagbox.tag02").eq(0).click()});
+/**
+ * @author      ZEUS Design - http://www.zeusdesign.com.tw
+ * @copyright   Copyright (c) 2016 ZEUS Design
+ */
+
+$(function () {
+
+  $('.Iaboutleft, .imgbox, .aboutbanner').imgLiquid ({verticalAlign: 'center'});
+
+  // about點選切換tab
+  $('.tagbox.tag01, .tagbox.tag02').click(function() {
+    $('.tagbox.tag01, .tagbox.tag02').removeClass ('Caboutac');
+    $(this).addClass ('Caboutac');
+
+    $('.Cabouttypebox').removeClass('Cabouttypeboxshow');
+    $('.Cabouttypebox').eq($(this).index()).addClass('Cabouttypeboxshow');
+  });
+
+  $('.tagbox.tag01, .tagbox.tag02').eq (0).click();
+
+});
